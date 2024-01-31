e4f33d7b8c912e76a99c791eb88798f9fbee47ae
Ковтун Захар <kovtun.zahar12@mail.ru>
2024-01-31 15:47:53 +0300

    BlogContext

---
+++ MvcStartApp/BlogContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MvcStartApp
+{
+    public sealed class BlogContext : DbContext
+    {
+        /// Ссылка на таблицу Users
+        public DbSet<User> Users { get; set; }
+
+        /// Ссылка на таблицу UserPosts
+        public DbSet<UserPost> UserPosts { get; set; }
+
+        public DbSet<Request> Requests { get; set; }
+
+        // Логика взаимодействия с таблицами в БД
+        public BlogContext(DbContextOptions<BlogContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
